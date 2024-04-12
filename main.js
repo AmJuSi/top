d0c0b1385a8b8ac75664e9a6c138a5fc3ff6681c
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-04-12 17:52:32 +0200

    Koordinaten und Zoom als Variable schreiben; Var map umschreiben

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-var map = L.map('map').setView([47.426944444444, 11.4216666666671771], 13);
+let lat = 47.426944444444;
+let lng = 11.4216666666671771;
+let zoom = 13;
+
+// var map = L.map('map').setView([lat, lng], zoom);
+
+var map = L.map('map', {
+    center: [lat, lng], // [] Array o. Liste, einträge mit komma getrennt
+    zoom: zoom
+});
+
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([47.426944444444, 11.4216666666671771]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+// var marker = L.marker([lat, lng]).addTo(map);
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
